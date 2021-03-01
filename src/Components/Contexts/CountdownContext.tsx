508b89632af4f54fd0ce79213b5646dbc827caa0
Gardiano <joaopaulong@hotmail.com>
2021-03-01 15:52:44 -0300

    add more minutes to challenge

--- src/Components/Contexts/CountdownContext.tsx
+++ src/Components/Contexts/CountdownContext.tsx
@@ -25,7 +25,7 @@ export function CountdownProvider ( {children} : CountdowProviderProps ) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
   // 25 minutos * 60 segundos - repesenta 25 minutos em segundos;
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
   const [isActive, setisActive] = useState(false);
@@ -39,7 +39,7 @@ export function CountdownProvider ( {children} : CountdowProviderProps ) {
     clearTimeout(countdownTimeout);
     setisActive(false);
     sethasFinished(false);
-    setTime(0.1 * 60);   
+    setTime(25 * 60);   
   }
 
   useEffect(() => {
